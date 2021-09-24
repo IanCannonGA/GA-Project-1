631a04b4e5239a494fa45dfe54bf673609808798
Ian Cannon <ian.cannon@protonmail.com>
2021-09-24 16:27:07 -0700

    Continue HTML/CSS layout, add placeholder images

--- js/project1.js
+++ js/project1.js
@@ -1,14 +1,38 @@
 /*----- constants -----*/
 const slotsReel = ['C', 'C', 'C', 'C', '$', '$', '$', '7', '7', 'JP'];
+const winConditions = {};
 
 /*----- app's state (variables) -----*/
 let credits, betpool, reel1, reel2, reel3;
 
 /*----- cached element references -----*/
-const reelDisp1 = document.querySelector('reel1');
-const reelDisp2 = document.querySelector('reel2');
-const reelDisp3 = document.querySelector('reel3');
+const reelDisp1 = document.getElementById('reel1');
+const reelDisp2 = document.getElementById('reel2');
+const reelDisp3 = document.getElementById('reel3');
+const credDisp = document.getElementById('credits');
+const betDisp = document.getElementById('betpool');
 
 /*----- event listeners -----*/
+document.getElementById('betup').addEventListener('click', betUp())
+document.getElementById('betdown').addEventListener('click', betDown())
+document.getElementById('lever').addEventListener('click', leverPull())
+
 
 /*----- functions -----*/
+init();
+
+function init() {
+
+}
+
+function betUp() {
+    console.log('betUp clicked!')
+}
+
+function betDown() {
+    console.log('betDown clicked!')
+}
+
+function leverPull() {
+    console.log('leverPull clicked!')
+}
