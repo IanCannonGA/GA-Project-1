410a3b59cdb88cd8b27683662cc620db79e4f4db
Ian Cannon <ian.cannon@protonmail.com>
2021-09-27 10:20:59 -0700

    Build out basic lever > reels-randomized function, remove placeholder HTML-CSS frames to show innerHTML changes

--- js/project1.js
+++ js/project1.js
@@ -3,40 +3,55 @@ const slotsReel = ['C', 'C', 'C', 'C', '$', '$', '$', '7', '7', 'JP'];
 const winConditions = {};
 
 /*----- app's state (variables) -----*/
-let credits, betpool, reel1, reel2, reel3;
+let credits, wager, reel1, reel2, reel3;
 
 /*----- cached element references -----*/
-const reelDisp1 = document.getElementById('reel1');
-const reelDisp2 = document.getElementById('reel2');
-const reelDisp3 = document.getElementById('reel3');
-const credDisp = document.getElementById('credits');
-const betDisp = document.getElementById('betpool');
+const reelDisp1 = document.getElementById('r1');
+const reelDisp2 = document.getElementById('r2');
+const reelDisp3 = document.getElementById('r3');
+const credDisp = document.getElementById('c-amount');
+const betDisp = document.getElementById('w-amount');
 
 /*----- event listeners -----*/
-document.getElementById('betup').addEventListener('click', betUp());
-document.getElementById('betdown').addEventListener('click', betDown());
-document.getElementById('lever').addEventListener('click', leverPull());
-document.getElementById('reset').addEventListener('click', reset());
-
-
+const bUpLis = document.getElementById('betup').addEventListener('click', betUp);
+const bDnLis = document.getElementById('betdown').addEventListener('click', betDown);
+const leverLis = document.getElementById('lever').addEventListener('click', leverPull);
+const resetLis = document.getElementById('reset').addEventListener('click', reset);
 
 /*----- functions -----*/
 init();
 
 function init() {
+    credits: 9;
+    wager: 1;
+    reel1: null;
+    reel2: null;
+    reel3: null;
+    render();
+}
+
+function render() {
+    reelDisp1.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
+    reelDisp2.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
+    reelDisp3.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
+    credDisp.innerHTML = `${credits}`;
+    betDisp.innerHTML = `${wager}`;
 
 }
 
-function betUp() {
+function betUp() { // subtract from credits, add to wager
     console.log('betUp clicked!');
 }
 
-function betDown() {
+function betDown() { // subtract from wager, add to credits
     console.log('betDown clicked!');
 }
 
 function leverPull() {
     console.log('leverPull clicked!');
+    reelDisp1.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
+    reelDisp2.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
+    reelDisp3.innerHTML = `${Math.floor(Math.random()*slotsReel.length)}`;
 }
 
 function reset() {
