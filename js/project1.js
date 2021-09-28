5e5dc753f74e49ea18a50748d4ffc01c1c0e0fba
Ian Cannon <ian.cannon@protonmail.com>
2021-09-28 10:45:12 -0700

    Update CSS/JS to include random-reel-image flash before timeout & array-generation function

--- js/project1.js
+++ js/project1.js
@@ -63,9 +63,12 @@ function leverPull() {
         credits--;
         wager++;
     };
-    reelDisp1.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
-    reelDisp2.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
-    reelDisp3.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
+    reelDisp1.innerHTML = '<img src="https://i.imgur.com/Botj2B8.gif">';
+    reelDisp2.innerHTML = '<img src="https://i.imgur.com/Botj2B8.gif">';
+    reelDisp3.innerHTML = '<img src="https://i.imgur.com/Botj2B8.gif">';
+    setTimeout(function() { reelDisp1.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`; }, 1000);
+    setTimeout(function() { reelDisp2.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`; }, 2000);
+    setTimeout(function() { reelDisp3.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`; }, 3000);
     reelArray.push(reelDisp1.innerHTML, reelDisp2.innerHTML, reelDisp3.innerHTML);
     console.log(reelArray);
     checkReels();
