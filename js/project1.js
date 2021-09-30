78b0ec2f87ffe045d2e48a2b31a8aa8458971415
Ian Cannon <ian.cannon@protonmail.com>
2021-09-30 10:24:15 -0700

    Polish code, look for reducibles and redundancies

--- js/project1.js
+++ js/project1.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-const virtualReel = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$2', '$2', '$2', '$2', '$2', '$2', '$2', '$3', '$3', '$3', '$3', 'L7', 'L7', 'L7', 'GB', 'GB', 'JP'];
+const vReel = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$2', '$2', '$2', '$2', '$2', '$2', '$2', '$3', '$3', '$3', '$3', 'L7', 'L7', 'L7', 'GB', 'GB', 'JP'];
 const imgLookup = {
     X: '<img src="https://i.imgur.com/q7sXusy.png">',
     C: '<img src="https://i.imgur.com/HmbaFJQ.png">',
@@ -22,15 +22,14 @@ const payouts = {
 };
 
 /*----- app's state (variables) -----*/
-let credits, wager, reelArray;
+let credits, wager, reelArr;
 
 /*----- cached element references -----*/
-const reelDisp1 = document.getElementById('r1');
-const reelDisp2 = document.getElementById('r2');
-const reelDisp3 = document.getElementById('r3');
+const rDisp1 = document.getElementById('r1');
+const rDisp2 = document.getElementById('r2');
+const rDisp3 = document.getElementById('r3');
 const credDisp = document.getElementById('c-amount');
 const betDisp = document.getElementById('w-amount');
-const leverCtrl = document.getElementById('lever');
 const rFlash = `${'<img src="https://i.imgur.com/zBUj2q0.gif">'}`;
 const betSnd = new Audio('audio/Bet1.mp3');
 const betAll = new Audio('audio/BetMax.mp3');
@@ -38,10 +37,10 @@ const leverSnd = new Audio('audio/leverPull.mp3');
 const reelSnd = new Audio('audio/reelSpin.mp3');
 
 /*----- event listeners -----*/
-const bUpLis = document.getElementById('betup').addEventListener('click', betUp);
-const bMaxLis = document.getElementById('betmax').addEventListener('click', betMax);
-const leverLis = document.getElementById('lever').addEventListener('click', leverPull);
-const resetLis = document.getElementById('reset').addEventListener('click', init);
+document.getElementById('betup').addEventListener('click', bet1);
+document.getElementById('betmax').addEventListener('click', betMax);
+document.getElementById('lever').addEventListener('click', leverPull);
+document.getElementById('reset').addEventListener('click', init);
 
 /*----- functions -----*/
 init();
@@ -49,20 +48,20 @@ init();
 function init() { //Only initiate tracked variables here
     credits = 10;
     wager = 0;
-    reelArray = ["L7", "L7", "L7"];
+    reelArr = ["L7", "L7", "L7"];
     render();
 }
 
 function render() { //Anything that goes on the PAGE goes here instead
-    reelDisp1.innerHTML = imgLookup[reelArray[0]];
-    reelDisp2.innerHTML = imgLookup[reelArray[1]];
-    reelDisp3.innerHTML = imgLookup[reelArray[2]];
+    rDisp1.innerHTML = imgLookup[reelArr[0]];
+    rDisp2.innerHTML = imgLookup[reelArr[1]];
+    rDisp3.innerHTML = imgLookup[reelArr[2]];
     credDisp.innerHTML = `${credits}`;
     betDisp.innerHTML = `${wager}`;
     leverCtrl.style.cursor = wager ? 'grab' : 'not-allowed';
 }
 
-function betUp() { // Add 1 Credit wager (to 3 max)
+function bet1() { // Add 1 Credit wager (to 3 max)
     if (wager < 3) {
         credits--;
         wager++;
@@ -82,32 +81,31 @@ function betMax() { // Bet maximum 3 Credits
 
 function leverPull() {
     if (wager === 0) return;
-    reelArray = [];
+    reelArr = [];
     leverSnd.play();
-    reelDisp1.innerHTML = rFlash;
-    reelDisp2.innerHTML = rFlash;
-    reelDisp3.innerHTML = rFlash;
+    rDisp1.innerHTML = rFlash;
+    rDisp2.innerHTML = rFlash;
+    rDisp3.innerHTML = rFlash;
     setTimeout(function() {
-        reelDisp1.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
+        rDisp1.innerHTML = `${Math.floor(Math.random()*vReel.length)}`;
     }, 750);
     setTimeout(function() {
-        reelDisp2.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
+        rDisp2.innerHTML = `${Math.floor(Math.random()*vReel.length)}`;
     }, 1500);
     setTimeout(function() {
-        reelDisp3.innerHTML = `${Math.floor(Math.random()*virtualReel.length)}`;
+        rDisp3.innerHTML = `${Math.floor(Math.random()*vReel.length)}`;
     }, 2250);
     setTimeout(function() {
         reelSnd.play();
-        reelArray.push(virtualReel[reelDisp1.innerHTML], virtualReel[reelDisp2.innerHTML], virtualReel[reelDisp3.innerHTML]);
+        reelArr.push(vReel[rDisp1.innerHTML], vReel[rDisp2.innerHTML], vReel[rDisp3.innerHTML]);
         checkReels();
         wager = 0;
-        if (credits === 0) alert("GAME OVER");
         render();
     }, 2251);
 }
 
 function checkReels() {
-    let reelCount = reelArray.reduce(function(total, sym) {
+    let reelCount = reelArr.reduce(function(total, sym) {
         total[sym] = total[sym] ? ++total[sym] : 1;
         return total;
     }, {});
@@ -122,10 +120,8 @@ function checkReels() {
     if (!foundWinner && reelCount.C === 1) {
         if (Object.keys(reelCount).length === 2) {
             credits += wager + wager * 5;
-            foundWinner = true;
         } else {
             credits += wager + wager * 2;
-            foundWinner = true;
         }
     }
     if (!foundWinner &&
@@ -138,6 +134,6 @@ function checkReels() {
             (reelCount.$1 === 1 && reelCount.$2 === 1 && reelCount.$3 === 1)
         )) {
         credits += wager + wager * 100;
-        foundWinner = true;
     }
+    if (credits === 0) alert("GAME OVER");
 }
