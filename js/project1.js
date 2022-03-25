b75bf15c7e5560fabd6c53ce768ea64ab9138f9a
Ian Cannon <ian.cannon@protonmail.com>
2022-03-25 15:16:52 -0700

    Add images to in-code folder

--- js/project1.js
+++ js/project1.js
@@ -1,14 +1,14 @@
 /*----- constants -----*/
 const vReel = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$1', '$2', '$2', '$2', '$2', '$2', '$2', '$2', '$3', '$3', '$3', '$3', 'L7', 'L7', 'L7', 'GB', 'GB', 'JP'];
 const imgLookup = {
-    X: '<img src="https://i.imgur.com/yyOTW20.png">',
-    C: '<img src="https://i.imgur.com/rjTgMxb.png">',
-    $1: '<img src="https://i.imgur.com/zZ19C0E.png">',
-    $2: '<img src="https://i.imgur.com/BEUePaz.png">',
-    $3: '<img src="https://i.imgur.com/x792iZk.png">',
-    L7: '<img src="https://i.imgur.com/Y7AymBZ.png">',
-    GB: '<img src="https://i.imgur.com/Dl4rbSY.png">',
-    JP: '<img src="https://i.imgur.com/2dmtO3m.png">',
+    X: '<img src="visual/X.png">',
+    C: '<img src="visual/cherries.png">',
+    $1: '<img src="visual/1$.png">',
+    $2: '<img src="visual/2$$.png">',
+    $3: '<img src="visual/3$$$.png">',
+    L7: '<img src="visual/lucky7.png">',
+    GB: '<img src="visual/goldbar.png">',
+    JP: '<img src="visual/moneybags.png">',
 }
 const payouts = {
     'JP': 1000,
@@ -30,7 +30,7 @@ const rDisp2 = document.getElementById('r2');
 const rDisp3 = document.getElementById('r3');
 const credDisp = document.getElementById('c-amount');
 const betDisp = document.getElementById('w-amount');
-const rFlash = `${'<img src="https://i.imgur.com/KmxcUzw.gif">'}`;
+const rFlash = `${'<img src="visual/spinningreel.gif">'}`;
 const leverCtrl = document.getElementById('lever');
 const betSnd = new Audio('audio/Bet1.mp3');
 const betAll = new Audio('audio/BetMax.mp3');
@@ -123,16 +123,16 @@ function checkReels() {
     }, {});
     for (let sym in reelCount) {
         if (reelCount[sym] === 3 && reelCount.X === 0) {
-            credits += wager + wager * payouts[sym];
+            credits += wager * payouts[sym];
             foundWinner = true;
             break;
         }
     }
     if (!foundWinner && reelCount.C === 1 && reelCount.X === 0) {
         if (Object.keys(reelCount).length === 2) {
-            credits += wager + wager * 5;
+            credits += wager * 5;
         } else {
-            credits += wager + wager * 2;
+            credits += wager * 2;
         }
     }
     if (!foundWinner &&
